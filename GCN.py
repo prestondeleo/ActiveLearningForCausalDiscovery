92ac7ca8665cae37afc1bcdaf7d06ba4a62fba9c
Preston DeLeo <121273342+prestondeleo@users.noreply.github.com>
2024-12-07 15:45:55 -0500

    adding loss functions for GCN model

--- GCN.py
+++ GCN.py
@@ -4,6 +4,7 @@ import math
 import torch_geometric
 import torch.nn as nn
 import torch.optim as optim
+import torch
 
 
 class GCN(nn.Module):
@@ -17,14 +18,20 @@ class GCN(nn.Module):
 
         pass
 
-    def reconstruction_loss(self):
-        pass
+    def reconstruction_loss(self, predicted_dag:np.ndarray, true_dag:np.ndarray):
+        loss = nn.functional.mse_loss(predicted_dag, true_dag)
+        return loss
 
-    def acyclic_loss(self, lambda_):
-        pass
+    def acyclic_loss(self, predicted_dag:np.ndarray):
+        d = predicted_dag.size(0)
+        exponentiation_matrix = torch.matrix_exp(predicted_dag * predicted_dag)
+        trace = torch.trace(exponentiation_matrix)
+        loss = trace - d
+        return loss
 
-    def full_loss(self):
-        pass
+    def full_loss(self, predicted_dag:np.ndarray, true_dag:np.ndarray, lambda_:float):
+        loss = self.reconstruction_loss(predicted_dag, true_dag) + lambda_ * self.acyclic_loss(predicted_dag)
+        return loss
 
     def train(self, epochs,pcdag:np.ndarray):
         pass
