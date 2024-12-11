876f1047c78fddb699abe54960a2005b7c0551a0
SeanBrownn <108001967+SeanBrownn@users.noreply.github.com>
2024-12-11 16:16:06 -0500

    added intervene method

--- active_learning.py
+++ active_learning.py
@@ -4,7 +4,7 @@ import networkx as nx
 import numpy as np
 import pandas as pd
 import random
-from dowhy import CausalModel
+#from dowhy import CausalModel
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from causallearn.utils.GraphUtils import GraphUtils
@@ -22,15 +22,19 @@ class Experiment:
     def get_neighbors(self, pcdag:np.ndarray, node:int)->np.ndarray:
         neighbors = np.where((pcdag[node, :] == 1))[0]
         return neighbors
-    
-    def intervene(self, data:pd.DataFrame, interv_node:int)->pd.DataFrame:
+
+    # gives interventional data for single vertex intervention
+    def intervene(self, true_causal_graph: nx.DiGraph, data:pd.DataFrame, interv_node:int)->pd.DataFrame:
         intervened_data = data.copy()
-        #intervened_data[interv_node] = 1
+
         intervened_data[interv_node] = 1
-        #intervened_data[interv_node] = np.random.uniform(low=-10, high=10, size=len(data))
-        """
-        We need to change this so updated intervention data has children of node effected and not children not eeffected I beleieev
-        """
+
+        # children of intervened node
+        child_indices = [int(child) for child in true_causal_graph.successors((interv_node))]
+
+        # multiplies values of children of intervened node by 100
+        #child_columns = intervened_data.iloc[:, child_indices]
+        intervened_data.iloc[:, child_indices] *= 100
 
         return intervened_data
     
@@ -38,7 +42,7 @@ class Experiment:
         pass
 
 
-    def visualize_pcdag(adj_matrix: np.ndarray, pos=None, title="Generated Graph", figsize=(10, 8)):
+    def visualize_pcdag(self, adj_matrix: np.ndarray, pos=None, title="Generated Graph", figsize=(10, 8)):
 
         G = nx.DiGraph()
         n_nodes = adj_matrix.shape[0]
@@ -60,7 +64,7 @@ class Experiment:
         
         return pos  
 
-    def random_dag_from_pcdag(pcdag_matrix):
+    def random_dag_from_pcdag(self, pcdag_matrix):
         np.random.seed(seed = 47)  
         random.seed(47)
         G = nx.DiGraph(pcdag_matrix)
@@ -86,9 +90,18 @@ class Experiment:
 if __name__ == '__main__':
         np.random.seed(seed = 47)  
         random.seed(47)
-        G = dg.create_dag(n = 5, expected_degree = 1)
+        G = dg.create_dag(n = 10, expected_degree = 3)
         start_adj_matrix = nx.to_numpy_array(G)        
         pcdag = pc_a.pc(G)
         experiment = Experiment(5, 5)
         true_DAG, DAG = experiment.random_dag_from_pcdag(pcdag) #gets random graph from MEC(s)
         data = dg.generate_data(graph = DAG, random_seed=47, num_rows=20000)
+
+        shared_pos = experiment.visualize_pcdag(pcdag, title="PCDAG")
+
+        experiment.visualize_pcdag(true_DAG, pos=shared_pos, title="true DAG")
+
+        #print([int(child) for child in DAG.successors(8)])
+
+        intervened_data = experiment.intervene(DAG, data, 8)
+        print(intervened_data.to_string())
